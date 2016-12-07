6502992430fed746822e07ee38048c602d3c2d93
Brett Morris <brettmorris21@gmail.com>
2016-12-06 19:30:16 -0800

    Updated likelihood w/ help from dfm

--- calibrate.py
+++ calibrate.py
@@ -17,8 +17,8 @@ cals_s_apo = Measurement([s.s_apo.uncalibrated.value for s in calstars],
                           err_lower=[s.s_apo.uncalibrated.err_lower for s in calstars])
 
 sort = np.argsort(cals_s_apo.value)
-plt.errorbar(cals_s_apo.value, cals_s_mwo.value,
-             xerr=cals_s_apo.err_lower, yerr=cals_s_mwo.err_lower, fmt='.')
+# plt.errorbar(cals_s_apo.value, cals_s_mwo.value,
+#              xerr=cals_s_apo.err_lower, yerr=cals_s_mwo.err_lower, fmt='.')
 
 ##############################################################################
 # Simple linear regression with errors in one dimension for initial fit
@@ -29,8 +29,8 @@ m_init, b_init = params
 
 init_model = m_init * cals_s_apo.value + b_init
 
-plt.plot(cals_s_apo.value[sort], init_model[sort],
-         color='gray', label='init model')
+# plt.plot(cals_s_apo.value[sort], init_model[sort],
+#          color='gray', label='init model')
 
 ##############################################################################
 # Wrote my own orthogonal distance regression likelihood for optimization
@@ -60,31 +60,80 @@ model_best = m_best * cals_s_apo.value + b_best
 
 final_likelihood = ln_likelihood(result, *args)
 print('final ln_likelihood', final_likelihood)
-print(result, m_best, b_best)
-plt.plot(cals_s_apo.value[sort], model_best[sort],
-         color='r', label='custom ODR')
-# plt.show()
 
-##############################################################################
-# Double check results with scipy's ODR package
-# ODR docs: https://docs.scipy.org/doc/external/odrpack_guide.pdf
-from scipy.odr import Model, RealData, ODR
+import emcee
+from corner import corner
 
-# Create a Model, create a RealData instance.
-linear = Model(lambda b, x: b[0] * x + b[1])
-mydata = RealData(cals_s_apo.value, cals_s_mwo.value,
-                  sx=cals_s_apo.err_lower, sy=cals_s_mwo.err_lower)
-myodr = ODR(mydata, linear, beta0=[m_init, b_init])
-myoutput = myodr.run()
-myoutput.pprint()
-m_odr, b_odr = myoutput.beta
+ndim, nwalkers = len(init_params), 10
+n_steps = 5000
+p0 = [[init_params[0] + 0.05 * np.random.randn(),
+       init_params[1] + 0.01 * np.random.randn(),
+       init_params[2] + 0.001 * np.random.randn()]
+      for i in range(nwalkers)]
 
-# Plot results
-model_odr = m_odr * cals_s_apo.value + b_odr
-plt.plot(cals_s_apo.value[sort], model_odr[sort],
-         color='g', ls='--', label='scipy ODR')
+sampler = emcee.EnsembleSampler(nwalkers, ndim, ln_likelihood, args=args,
+                                threads=3)
+p1 = sampler.run_mcmc(p0, 100)[0]
+sampler.reset()
+sampler.run_mcmc(p1, n_steps)
 
-plt.legend(loc='lower right')
+samples = sampler.chain[:, 1000:, :].reshape((-1, ndim))
+
+# Convert theta parameter to slope:
+samples[:, 0] = np.tan(samples[:, 0])
+# Convert lnf to f
+samples[:, -1] = np.exp(samples[:, -1])
+
+m_mcmc, b_mcmc, f_mcmc = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
+                             zip(*np.percentile(samples, [16, 50, 84],
+                                                axis=0)))
+
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.plot(cals_s_apo.value[sort], model_best[sort],
+        color='r', label='MCMC model')
+
+ax.errorbar(cals_s_apo.value, cals_s_mwo.value,
+            xerr=f_mcmc[0] * cals_s_apo.err_lower,
+            yerr=cals_s_mwo.err_lower, fmt='.', color='k',
+            ecolor='gray', capsize=0)
+
+fontsize = 16
+ax.set_xlabel("$S_{APO}$", fontsize=fontsize)
+ax.set_ylabel("$S_{MWO}$", fontsize=fontsize)
+
+note = ("$C_1 = {0:.2f}^{{+ {1:.2f} }}_{{- {2:.2f} }}$\n".format(*m_mcmc) +
+        "$C_2 = {0:.2f}^{{+ {1:.2f} }}_{{- {2:.2f} }}$".format(*b_mcmc))
+
+ax.text(0.05, 0.1, note, ha='right', fontsize=fontsize)
+ax.set_aspect('auto', 'datalim')
+
+fig.savefig('plots/s-index_calibration.png', bbox_inches='tight', dpi=200)
+
+corner(samples, labels=[r'$C_1$', '$C_2$', '$f$'])
+
+plt.savefig('plots/corner.png', bbox_inches='tight', dpi=200)
 plt.show()
 
-
+# ##############################################################################
+# # Double check results with scipy's ODR package
+# # ODR docs: https://docs.scipy.org/doc/external/odrpack_guide.pdf
+# from scipy.odr import Model, RealData, ODR
+#
+# # Create a Model, create a RealData instance.
+# linear = Model(lambda b, x: b[0] * x + b[1])
+# mydata = RealData(cals_s_apo.value, cals_s_mwo.value,
+#                   sx=cals_s_apo.err_lower, sy=cals_s_mwo.err_lower)
+# myodr = ODR(mydata, linear, beta0=[m_init, b_init])
+# myoutput = myodr.run()
+# myoutput.pprint()
+# m_odr, b_odr = myoutput.beta
+#
+# # Plot results
+# model_odr = m_odr * cals_s_apo.value + b_odr
+# plt.plot(cals_s_apo.value[sort], model_odr[sort],
+#          color='g', ls='--', label='scipy ODR')
+#
+# plt.legend(loc='lower right')
+# plt.show()
+#
+#
--- toolkit/analysis.py
+++ toolkit/analysis.py
@@ -2,7 +2,6 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 def v_vector(theta):
@@ -17,22 +16,22 @@ def ln_likelihood(p, x, y, x_err, y_err):
     Hogg+ 2010, Eqn 30., modified to allow x_err to float
     """
     theta, b, lnf = p
+
+    if theta < 1.4 or theta > 1.6 or lnf < -5:
+        return -np.inf
+
     z = np.array([x, y])
     v = v_vector(theta)
+    f = np.exp(lnf)
 
     ln_like = 0
     for i in range(len(x)):
         z_i = np.atleast_2d(z[:, i]).T
         delta = np.dot(v.T, z_i) - b * np.cos(theta)
-        s = np.array([[x_err[i]**2, 0], [0, y_err[i]**2]])
+        s = np.array([[(f * x_err[i])**2, 0], [0, y_err[i]**2]])
         sigma_sq = np.dot(np.dot(v.T, s), v)
-
-        # model = np.dot(v, np.array([x[i], y[i]]).T) - b * np.cos(theta)
-        # inv_sigma2 = 1.0/(sigma_sq + model**2 * np.exp(2*lnf))
-        # ln_like -= (delta**2 / 2 * inv_sigma2 - np.log(inv_sigma2))
-        # model = np.dot(v, np.array([x[i], y[i]]).T) - b * np.cos(theta)
-
-        ln_like -= delta**2 / 2 / sigma_sq
+        ln_like -= 0.5 * ((delta**2 / sigma_sq) + np.log(sigma_sq) +
+                          np.log(2*np.pi))
 
     return ln_like[0]
 
