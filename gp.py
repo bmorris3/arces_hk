77cba49b83f3696d8e2cb9f8d6b835e271300a16
Brett Morris <brettmorris21@gmail.com>
2016-12-11 18:15:13 -0800

    Using Duncan+ 1991 for S min prior

--- gp.py
+++ gp.py
@@ -1,13 +1,16 @@
 import george
-from george.kernels import CosineKernel, Matern32Kernel
+from george.kernels import Matern32Kernel
 import numpy as np
-from toolkit import json_to_stars
+from toolkit import json_to_stars, get_duncan_catalog
 import matplotlib.pyplot as plt
 import emcee
 from corner import corner
+from astropy.time import Time
 
 hat11_keck = json_to_stars('hat11_keck_calibrated.json')
 hat11_apo = json_to_stars('hat11_apo_calibrated.json')
+
+catalog_s_min = get_duncan_catalog()['Smin'].data.min()
 
 x = []
 y = []
@@ -26,22 +29,23 @@ y = np.array(y)[sort] #- np.mean(y)
 yerr = np.array(yerr)[sort]
 
 
-def model(p, x, y, yerr):
-    s0, alpha, per, t0, lna, tau, lns = p
+def model(p, x):
+    s0, alpha, per, t0, lna, tau = p
     return s0 + alpha * np.sin(2*np.pi/per * (x - t0))
 
 
 def lnlike_gp(p, x, y, yerr):
-    s0, alpha, per, t0, lna, tau, lns = p
+    s0, alpha, per, t0, lna, tau = p
     gp = george.GP(np.exp(lna) * Matern32Kernel(tau))
-    gp.compute(x, np.sqrt(yerr ** 2 + np.exp(2 * lns)))
-    return gp.lnlikelihood(y - model(p, x, y, yerr))
+    gp.compute(x, yerr)
+    return gp.lnlikelihood(y - model(p, x))
 
 
 def lnprior(p):
-    s0, alpha, per, t0, lna, tau, lns = p
-    if not ((alpha < s0 < y.max()) and (5*365 < per < 50*365) and (lns < 5)
-            and (-50 < lna < 10) and (1 < tau < 200) and (alpha > 0)):
+    s0, alpha, per, t0, lna, tau = p
+    if not ((alpha < s0 < y.max()) and (5*365 < per < 50*365) and
+            (-50 < lna < 10) and (1 < tau < 200) and (alpha > 0) and
+            s0-alpha >= catalog_s_min):
         return -np.inf
     else:
         return 0
@@ -59,7 +63,7 @@ def fit_gp(initial, data, nwalkers=16):
 
     p0 = []
 
-    s0, alpha, per, t0, lna, tau, lns = initial
+    s0, alpha, per, t0, lna, tau = initial
 
     while len(p0) < nwalkers:
 
@@ -68,8 +72,7 @@ def fit_gp(initial, data, nwalkers=16):
                     per + 300 * np.random.randn(),
                     t0 + 50 * np.random.randn(),
                     lna + 0.1 * np.random.randn(),
-                    tau + 1 * np.random.randn(),
-                    lns + 0.1 * np.random.randn()]
+                    tau + 1 * np.random.randn()]
         if not np.isfinite(lnprior(p0_trial)):
             p0.append(p0_trial)
 
@@ -87,16 +90,16 @@ def fit_gp(initial, data, nwalkers=16):
     sampler.reset()
 
     print("Running production")
-    p0, _, _ = sampler.run_mcmc(p0, 2000)
+    p0, _, _ = sampler.run_mcmc(p0, 1000)
 
     return sampler
 
 
-init_guess = [0.4, 0.275, 30*365, 2571990.5, -7.2, 45, -4.5]
+init_guess = [0.4, 0.275, 30*365, 2571990.5, -7.2, 45]
 args = (x, y, yerr)
 
 # Set up the Gaussian process.
-s0, alpha, per, t0, lna, tau, lns = init_guess
+s0, alpha, per, t0, lna, tau = init_guess
 
 kernel = np.exp(lna) * Matern32Kernel(tau)
 gp = george.GP(kernel)
@@ -107,14 +110,14 @@ gp.compute(x, yerr)
 # Compute the log likelihood.
 
 t = np.linspace(x.min(), x.max(), 5000)
-mu, cov = gp.predict(y - model(init_guess, x, y, yerr), t)
-mu += model(init_guess, t, y, yerr)
+mu, cov = gp.predict(y - model(init_guess, x), t)
+mu += model(init_guess, t)
 std = np.sqrt(np.diag(cov))
 
-plt.figure()
-plt.plot(t, mu)
-plt.errorbar(x, y, yerr=yerr, fmt='.', color='k', ecolor='gray')
-plt.show()
+# plt.figure()
+# plt.plot(t, mu)
+# plt.errorbar(x, y, yerr=yerr, fmt='.', color='k', ecolor='gray')
+# plt.show()
 
 sampler = fit_gp(init_guess, args)
 
@@ -122,21 +125,42 @@ samples = sampler.flatchain
 
 fig, ax = plt.subplots(samples.shape[1], samples.shape[1], figsize=(8, 8))
 corner(samples, fig=fig,
-       labels=['s0', 'alpha', 'per', 't0', 'lna', 'tau', 'lns'])
+       labels=['s0', 'alpha', 'per', 't0', 'lna', 'tau'])
+fig.savefig('plots/corner.png', bbox_inches='tight', dpi=200)
 plt.show()
-t = np.linspace(x.min(), x.max()+x.ptp(), 1500)
+#t = np.linspace(x.min(), x.max()+x.ptp(), 1500)
+
+t = np.concatenate((x, np.linspace(x.min(), x.max()+x.ptp(), 500)))
+t = np.sort(t)
 
 plt.figure()
-for s in samples[np.random.randint(len(samples), size=50)]:
-    s0, alpha, per, t0, lna, tau, lns = s
+for s in samples[np.random.randint(len(samples), size=250)]:
+    s0, alpha, per, t0, lna, tau = s
 
     gp = george.GP(np.exp(lna) * Matern32Kernel(tau))
-    gp.compute(x, np.sqrt(yerr**2 + np.exp(2*lns)))
-    m = gp.sample_conditional(y - model(s, x, y, yerr), t) + model(s, t, y, yerr)
-    plt.plot(t, m, color="#4682b4", alpha=0.15)
-plt.errorbar(x, y, yerr=yerr, fmt=".k", capsize=0)
-plt.ylabel(r"$y$")
-plt.xlabel(r"$t$")
-plt.xlim(t.min(), t.max())
-plt.title("results with Gaussian process noise model")
+    gp.compute(x, yerr)
+    m = gp.sample_conditional(y - model(s, x), t) + model(s, t)
+    plt.plot_date(Time(t, format='jd').plot_date, m, '-',
+                  color="#4682b4", alpha=0.05)
+plt.errorbar(Time(x, format='jd').plot_date, y, yerr=yerr, fmt=".k", capsize=0)
+plt.ylabel(r"$S$-index")
+#plt.xlim(t.min(), t.max())
+plt.title(" Gaussian process model")
+plt.savefig('plots/model.png', bbox_inches='tight', dpi=200)
+plt.show()
+
+rescaled_samples = np.copy(samples)
+rescaled_samples[:, 2] /= 365
+rescaled_samples[:, 3] -= rescaled_samples[:, 3].mean()
+rescaled_samples[:, 3] /= 365
+
+fig, ax = plt.subplots(rescaled_samples.shape[1],
+                       rescaled_samples.shape[1], figsize=(8, 8))
+corner(rescaled_samples, fig=fig,
+       labels=['$S_0$', r'$\alpha$', '$P$ [years]', '$t_0$ [years]',
+               r'$\ln{a}$', r'$\tau$'])
+suptitle = (r'$S(t) = S_0 + \alpha \, \sin\left(\frac{2\pi}{P} (t - t_0)\right),$' +
+            '\n' + r'$k(x_i, x_j) = a\,$Matern3/2($\tau$)')
+fig.suptitle(suptitle, fontsize=15)
+fig.savefig('plots/corner_scaled.png', bbox_inches='tight', dpi=200)
 plt.show()
