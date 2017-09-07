f04b4810550c5a8d48d8ad59633f9277912b8c13
Brett Morris <brettmorris21@gmail.com>
2017-09-06 19:20:54 -0700

    Removing extra files

--- get_mean_mwo_error.py
+++
@@ -1,8 +0,0 @@
-from toolkit import get_duncan_catalog
-
-table = get_duncan_catalog()
-errors = table['e_Smean'].data.data
-nonzero_errors = errors[errors != 0]
-
-print('Mean error on an MWO S-index measurement: {}'
-      .format(nonzero_errors.mean()))
